8e0095246716d87ca15f4dabc723ab08f0077371
aaronli16 <liaaron53@gmail.com>
2026-02-12 11:59:08 -0800

    Initial Commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Aaron Li on 2/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
