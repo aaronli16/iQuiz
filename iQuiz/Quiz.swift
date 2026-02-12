b16c7bda288c2ff101a8114ecb5e2f6cb48baeac
aaronli16 <liaaron53@gmail.com>
2026-02-12 12:48:30 -0800

    Finished part1

---
+++ iQuiz/Quiz.swift
@@ -0,0 +1,15 @@
+//
+//  Quiz.swift
+//  iQuiz
+//
+//  Created by Aaron Li on 2/12/26.
+//
+
+
+import Foundation
+
+struct Quiz {
+    let title: String
+    let description: String
+    let iconName: String
+}
--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -7,11 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    var quizzes: [Quiz] = [
+            Quiz(title: "EDM", description: "Test your electronic music knowledge!", iconName: "edm"),
+            Quiz(title: "Startups", description: "How well do you know startup culture?", iconName: "startups"),
+            Quiz(title: "Business", description: "Master the fundamentals of business!", iconName: "business")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    @IBAction func settingsButtonTapped(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Settings", message: "Settings go here", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quizzes.count
+    }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuizCell", for: indexPath)
+        let quiz = quizzes[indexPath.row]
+        
+        cell.textLabel?.text = quiz.title
+        cell.detailTextLabel?.text = quiz.description
+        
+        // icons
+        if indexPath.row == 0 {
+            cell.imageView?.image = UIImage(systemName: "music.note.list")
+        } else if indexPath.row == 1 {
+            cell.imageView?.image = UIImage(systemName: "lightbulb.fill")
+        } else {
+            cell.imageView?.image = UIImage(systemName: "chart.line.uptrend.xyaxis")
+        }
+        
+        return cell
     }
 
 
